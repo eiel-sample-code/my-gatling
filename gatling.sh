bf8dfb4e8b1b7a8ac8fd22abfe514b3f96b07c31
Tomohiko Himura <eiel.hal@gmail.com>
2016-12-16 02:12:28 +0900

    serfからgatlingを実行するshellを作成

---
+++ gatling.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+export TARGET_ADDRESS=10.146.0.4
+export GATLING_GRAPHITE_HOST=10.146.0.5
+
+sbt gatling:test \
+  -Dgatling.application.baseUrl=http://${TARGET_ADDRESS} \
+  -Dgatling.application.pasueTime=1ms \
+  -Dgatling.application.rampUsers=50000 \
+  -Dgatling.application.duration=5m
