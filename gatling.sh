6a441c22432496ae20e44a7894b4c881158c0090
Tomohiko Himura <eiel.hal@gmail.com>
2016-12-17 01:37:13 +0900

    sbtがserfで停止しないように対策

--- gatling.sh
+++ gatling.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 export TARGET_ADDRESS=10.146.0.4
 export GATLING_GRAPHITE_HOST=10.146.0.5
 
-sbt gatling:test \
+# sbt が停止してしまうので対策
+screen -dm -S gat bash -c "sbt gatling:test \
   -Dgatling.application.baseUrl=http://${TARGET_ADDRESS} \
-  -Dgatling.application.pasueTime=1ms \
+  -Dgatling.application.pasueTime=1s \
   -Dgatling.application.rampUsers=50000 \
-  -Dgatling.application.duration=5m \
-  > /dev/null
+  -Dgatling.application.duration=3m &> /dev/null"
