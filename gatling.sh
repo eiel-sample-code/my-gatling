8d106811b90d6a04b6ef133f7de88f335b341906
Tomohiko Himura <eiel.hal@gmail.com>
2016-12-17 01:45:57 +0900

    微調整

--- gatling.sh
+++ gatling.sh
@@ -9,5 +9,5 @@ export GATLING_GRAPHITE_HOST=10.146.0.5
 screen -dm -S gat bash -c "sbt gatling:test \
   -Dgatling.application.baseUrl=http://${TARGET_ADDRESS} \
   -Dgatling.application.pasueTime=1s \
-  -Dgatling.application.rampUsers=50000 \
-  -Dgatling.application.duration=3m &> /dev/null"
+  -Dgatling.application.rampUsers=2000 \
+  -Dgatling.application.duration=2m &> /dev/null"
